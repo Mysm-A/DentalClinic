7048692fe065f2db319fdaaaf83e4378c4f7b43c
Mysm-A <159068299+Mysm-A@users.noreply.github.com>
2025-09-02 17:24:03 +0300

    update

--- src/Dentalclinic.js
+++ src/Dentalclinic.js
@@ -10,27 +10,26 @@ function Dentalclinic() {
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
-
   const [message, setMessage] = useState('');
-
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (name && date) {
       setMessage(`Your appointment has been successfully booked! Name: ${name}, Date: ${date}`);
       setName('');
       setDate('');
       setTime('');
-    } else {
+         } 
+    else {
       setMessage('Please fill in all fields!');
     }
   };
-
   
 return (
       <div>
-        {}
+        
       <div className='burger-menu'>
         <div className='burger-icon' onClick={toggleMenu}> &#9776;
           {menuOpen && (
@@ -43,14 +42,15 @@ return (
           </div>
         </div>
 {/* home */}
-        <div id='home' className='heroSection' style={{backgroundImage: `url(${heroImage})` }}>
 
+        <div id='home' className='heroSection' style={{backgroundImage: `url(${heroImage})` }}>
           <div className='overlay'><h1 className='clinic-title'>HAPPY SMILE CLINIC </h1>
           <button className='hero-button' onClick={() => document.getElementById('appointment').scrollIntoView({ behavior: 'smooth' })} >
-               Book Your Consultation Now ▼</button>
+            Book Your Consultation Now ▼</button>
           </div>
         </div>
 {/* about */}
+
         <div id='about' className='about-section' style={{backgroundImage: `url(${heImage})` }}>
           <h2>About Us</h2>
           <p>
@@ -58,8 +58,8 @@ return (
           with the latest technology and a caring team to ensure your best smile.
         </p>
         </div>
-       
 
+{/* appointment */}
           <div id='appointment' className='dental-container' style={{backgroundImage: `url(${herImage})` }}>
             <h2>Take Your Appointment</h2>
             <form onSubmit={handleSubmit} className="appointment-form">
@@ -67,28 +67,23 @@ return (
               placeholder='Full Name'
               value={name}
               onChange={(e) => setName(e.target.value)} 
-              /> 
-              
+           /> 
               <input type='date'
               value={date}
               onChange={(e)=> setDate(e.target.value)}
-              />
+           />
+
          <input
             type="time"
             value={time}
             onChange={(e) => setTime(e.target.value)}
             
-          />
-
-
+           />
               <button type='submit'>SEND</button>
-            </form>
-                    {/* عرض رسالة النجاح */}
-
+              </form>
           {message && <p className="success-message">{message}</p>}
       </div>
     </div>
-
   );
 }
 export default Dentalclinic;
